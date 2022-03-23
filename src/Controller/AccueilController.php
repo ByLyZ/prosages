58d2b93806a7ca9be24be14b53f8f5978892ea4d
DUPONT Bastien <bdupont001iutbayonne.univ-pau.fr>
2022-03-23 21:43:18 +0100

    Fin formulaire d'inscription

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\StagesRepository;
 use App\Entity\Stages;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Form\UserType;
+use App\Entity\User;
 
 class AccueilController extends AbstractController
 {
@@ -18,5 +23,26 @@ class AccueilController extends AbstractController
         $stages = $repositoryStages->AfficherToutStage();
 
         return $this->render('accueil/index.html.twig', ['controller_name' => 'AccueilController','stages' => $stages]);
+    }
+        /**
+     * @Route("/inscription", name="ajouter_user")
+     */
+    public function ajouterUser(Request $request, EntityManagerInterface $manager): Response
+    {
+        $user=new User();
+
+        $formulaireUser = $this->createForm(UserType::class, $user);
+
+        $formulaireUser->handleRequest($request);
+        
+        if($formulaireUser->isSubmitted() && $formulaireUser->isValid())
+        {
+            //$manager->persist($user);
+            //$manager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/userAjouter.html.twig',['vueFormulaire' => $formulaireUser->createView()]);
     }
 }
---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Form\UserType;
+use App\Entity\User;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/inscription", name="ajouter_user")
+     */
+    public function ajouterUser(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder): Response
+    {
+        $user=new User();
+
+        $formulaireUser = $this->createForm(UserType::class, $user);
+
+        $formulaireUser->handleRequest($request);
+        
+        if($formulaireUser->isSubmitted() && $formulaireUser->isValid())
+        {
+            $user->setRoles(["ROLE_USER"]);
+
+            $encodagePassword = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($encodagePassword);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/userAjouter.html.twig',['vueFormulaire' => $formulaireUser->createView()]);
+    }
+}
